2424ca67627f3aeb6eae5d8c457c44a27e12942e
vitorhes <90984475+vitorhes@users.noreply.github.com>
2022-02-11 12:04:03 -0300

    Update cleaning.py

--- cleaning.py
+++ cleaning.py
@@ -5,11 +5,12 @@ def read_and_clean_results():
     df["date"] = df["date"].str.replace("-","/")
     df.dropna(inplace = True)
     df.to_csv(r"C:\Users\Public\results_clean.csv",index = False)
-read_and_clean_results()
+
 
 
 def read_and_clean_shootouts():
     df = pd.read_csv("shootouts.csv")
     df.dropna(inplace = True)
     df.to_csv(r"C:\Users\Public\shootouts_clean.csv",index = False)
-read_and_clean_shootouts()
+
+    
